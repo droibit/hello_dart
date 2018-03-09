406cd883a71aec205a2152fc0654a4f9ff1cd07f
Shinya Kumagai <roomful.rooms@gmail.com>
2018-03-09 14:14:42 +0900

    Add vector_math  to dependencies

--- example/hello_dart_example.dart
+++ example/hello_dart_example.dart
@@ -1,10 +1,14 @@
 import 'package:hello_dart/hello_dart.dart';
+import 'package:hello_lib/hello_lib.dart';
 import 'package:vector_math/vector_math.dart';
 
 
 main() {
   var awesome = new Awesome();
   print('awesome: ${awesome.isAwesome}');
+
+  var awesomeLib = new AwesomeLibrary();
+  print('awesomeLibrary: ${awesomeLib.isAwesome}');
 
   final v2 = new Vector2(1.0, 2.0);
   print("v2: $v2");
